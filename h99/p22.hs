4977d7ab94584455220f19e48851aca539c2849b
tkuriyama <taro.kuriyama@gmail.com>
2020-09-04 09:27:16 -0400

    Adding p24

--- h99/p22.hs
+++ h99/p22.hs
@@ -10,3 +10,13 @@ range a b = [a..b]
 range' :: Int -> Int -> [Int]
 range' a b = take (b-a+1) $ iterate (\n -> n+1) a
                        
+
+
+import System.IO
+import System.Random (randomRIO)
+import Control.Monad (replicateM)
+
+rndSelect :: [a] -> Int -> IO [a]  
+rndSelect l n = do
+    is <- replicateM n $ randomRIO (0, length l - 1)
+    return $ map (\i -> l !! i) is
