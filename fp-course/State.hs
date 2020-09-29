58e3ac7192972b30dc934f49ec1fc9bc6cf19a63
tkuriyama <taro.kuriyama@gmail.com>
2020-09-29 18:13:00 -0400

    adding State

---
+++ fp-course/State.hs
@@ -0,0 +1,213 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE RebindableSyntax #-}
+
+module Course.State where
+
+import Course.Core
+import qualified Prelude as P
+import Control.Arrow ((&&&))
+import Course.Optional
+import Course.List
+import Course.Functor
+import Course.Applicative
+import Course.Monad
+import qualified Data.Set as S
+import Data.Char (digitToInt)
+
+-- $setup
+-- >>> import Test.QuickCheck.Function
+-- >>> import Data.List(nub)
+-- >>> import Test.QuickCheck
+-- >>> import qualified Prelude as P(fmap)
+-- >>> import Course.Core
+-- >>> import Course.List
+-- >>> instance Arbitrary a => Arbitrary (List a) where arbitrary = P.fmap listh arbitrary
+
+-- A `State` is a function from a state value `s` to (a produced value `a`, and a resulting state `s`).
+newtype State s a =
+  State {
+    runState ::
+      s
+      -> (a, s)
+  }
+
+-- | Run the `State` seeded with `s` and retrieve the resulting state.
+--
+-- prop> \(Fun _ f) s -> exec (State f) s == snd (runState (State f) s)
+exec ::
+  State s a
+  -> s
+  -> s
+exec (State f) s = snd $ f s
+
+-- | Run the `State` seeded with `s` and retrieve the resulting value.
+--
+-- prop> \(Fun _ f) s -> eval (State f) s == fst (runState (State f) s)
+eval ::
+  State s a
+  -> s
+  -> a
+eval (State f) s = fst $ f s
+  
+
+-- | A `State` where the state also distributes into the produced value.
+--
+-- >>> runState get 0
+-- (0,0)
+get ::
+  State s s
+get = State $ \s -> (s, s)
+
+
+-- | A `State` where the resulting state is seeded with the given value.
+--
+-- >>> runState (put 1) 0
+-- ((),1)
+put ::
+  s
+  -> State s ()
+put x = State $ \s -> ((), x)
+
+-- | Implement the `Functor` instance for `State s`.
+--
+-- >>> runState ((+1) <$> State (\s -> (9, s * 2))) 3
+-- (10,6)
+instance Functor (State s) where
+  (<$>) ::
+    (a -> b)
+    -> State s a
+    -> State s b
+  (<$>) f g = State $ \s -> (f $ eval g s, exec g s)
+
+-- | Implement the `Applicative` instance for `State s`.
+--
+-- >>> runState (pure 2) 0
+-- (2,0)
+--
+-- >>> runState (pure (+1) <*> pure 0) 0
+-- (1,0)
+--
+-- >>> import qualified Prelude as P
+-- >>> runState (State (\s -> ((+3), s P.++ ["apple"])) <*> State (\s -> (7, s P.++ ["banana"]))) []
+-- (10,["apple","banana"])
+instance Applicative (State s) where
+  pure ::
+    a
+    -> State s a
+  pure a = State $ \s -> (a, s)
+  (<*>) ::
+    State s (a -> b)
+    -> State s a
+    -> State s b 
+  (<*>) (State f) g = State $ \s -> let (a, newState) = f s
+                                        (State h) = a <$> g
+                                    in h newState
+  
+-- | Implement the `Bind` instance for `State s`.
+--
+-- >>> runState ((const $ put 2) =<< put 1) 0
+-- ((),2)
+--
+-- >>> let modify f = State (\s -> ((), f s)) in runState (modify (+1) >>= \() -> modify (*2)) 7
+-- ((),16)
+instance Monad (State s) where
+  (=<<) ::
+    (a -> State s b)
+    -> State s a
+    -> State s b
+  (=<<) f (State g) = State $ \s -> let (a, newState) = g s
+                                        (State h) = f a
+                                    in h newState
+
+-- | Find the first element in a `List` that satisfies a given predicate.
+-- It is possible that no element is found, hence an `Optional` result.
+-- However, while performing the search, we sequence some `Monad` effect through.
+--
+-- Note the similarity of the type signature to List#find
+-- where the effect appears in every return position:
+--   find ::  (a ->   Bool) -> List a ->    Optional a
+--   findM :: (a -> f Bool) -> List a -> f (Optional a)
+--
+-- >>> let p x = get >>= (\s -> (const $ pure (x == 'c')) =<< put (1+s))
+--     in runState (findM p $ listh ['a'..'h']) 0
+-- (Full 'c',3)
+--
+-- >>> let p x = (\s -> (const $ pure (x == 'i')) =<< put (1+s)) =<< get
+  --   in runState (findM p $ listh ['a'..'h']) 0
+-- (Empty,8)
+
+findM :: forall f a . Monad f => (a -> f Bool) -> List a -> f (Optional a)
+findM p = foldRight test (pure Empty)
+  where
+    test :: Monad f => a -> f (Optional a) -> f (Optional a)
+    test x acc = p x >>= (\t -> if t then pure (Full x) else acc)
+
+findM' :: forall f a . Monad f => (a -> f Bool) -> List a -> f (Optional a)
+findM' p = extract . pairs p
+
+pairs :: (a -> f Bool) -> List a -> List (a, f Bool)
+pairs p = map (id &&& p)
+
+extract :: Monad f => List (a, f Bool) -> f (Optional a)
+extract = foldRight test' (pure Empty)
+  where test' (a, fb) acc = fb >>= \b -> if b then pure (Full a) else acc 
+          --(\b ma -> if b then (Full a) else ma) <$> fb <*> acc
+          
+-- headOr Empty <$> map Full <$> filtering p xs
+
+
+-- | Find the first element in a `List` that repeats.
+-- It is possible that no element repeats, hence an `Optional` result.
+--
+-- /Tip:/ Use `findM` and `State` with a @Data.Set#Set@.
+--
+-- prop> \xs -> case firstRepeat xs of Empty -> let xs' = hlist xs in nub xs' == xs'; Full x -> length (filter (== x) xs) > 1
+-- prop> \xs -> case firstRepeat xs of Empty -> True; Full x -> let (l, (rx :. rs)) = span (/= x) xs in let (l2, r2) = span (/= x) rs in let l3 = hlist (l ++ (rx :. Nil) ++ l2) in nub l3 == l3
+firstRepeat :: Ord a => List a -> Optional a
+firstRepeat xs = fst $ runState (findM p xs) S.empty 
+  where p x = do 
+          s <- get
+          s' <- put (S.insert x s)
+          pure (x `S.member` s)
+
+-- | Remove all duplicate elements in a `List`.
+-- /Tip:/ Use `filtering` and `State` with a @Data.Set#Set@.
+--
+-- prop> \xs -> firstRepeat (distinct xs) == Empty
+--
+-- prop> \xs -> distinct xs == distinct (flatMap (\x -> x :. x :. Nil) xs)
+distinct :: Ord a => List a -> List a
+distinct xs = fst $ runState (filtering p xs) S.empty
+  where p a = do
+          s <- get
+          s' <- put (S.insert a s)
+          pure (not $ a `S.member` s)
+
+-- | A happy number is a positive integer, where the sum of the square of its digits eventually reaches 1 after repetition.
+-- In contrast, a sad number (not a happy number) is where the sum of the square of its digits never reaches 1
+-- because it results in a recurring sequence.
+--
+-- /Tip:/ Use `firstRepeat` with `produce`.
+--
+-- /Tip:/ Use `join` to write a @square@ function.
+--
+-- /Tip:/ Use library functions: @Optional#contains@, @Data.Char#digitToInt@.
+--
+-- >>> isHappy 4
+-- False
+--
+-- >>> isHappy 7
+-- True
+--
+-- >>> isHappy 42
+-- False
+--
+-- >>> isHappy 44
+-- True
+isHappy :: Integer -> Bool
+isHappy n = contains 1 $ firstRepeat $ generate n
+  where generate n = produce f n
+        f = toInteger . sum . squareDigits
+        squareDigits = map (join (*) . digitToInt) . listh . show
