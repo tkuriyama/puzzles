2466a7ec0777cd8b71ce6e981c8e9a9f180b45af
tkuriyama <taro.kuriyama@gmail.com>
2020-10-08 10:07:28 -0400

    adding Extend

---
+++ fp-course/Extend.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+
+module Course.Extend where
+
+import Course.Core
+import Course.ExactlyOne
+import Course.List
+import Course.Optional
+import Course.Functor
+
+-- | All instances of the `Extend` type-class must satisfy one law. This law
+-- is not checked by the compiler. This law is given as:
+--
+-- * The law of associativity
+--   `∀f g. (f <<=) . (g <<=) ≅ (<<=) (f . (g <<=))`
+class Functor f => Extend f where
+  -- Pronounced, extend.
+  (<<=) ::
+    (f a -> b)
+    -> f a
+    -> f b
+
+infixr 1 <<=
+
+-- | Implement the @Extend@ instance for @ExactlyOne@.
+--
+-- >>> id <<= ExactlyOne 7
+-- ExactlyOne (ExactlyOne 7)
+instance Extend ExactlyOne where
+  (<<=) ::
+    (ExactlyOne a -> b)
+    -> ExactlyOne a
+    -> ExactlyOne b
+  (<<=) f a = ExactlyOne (f a)
+
+-- | Implement the @Extend@ instance for @List@.
+--
+-- >>> length <<= ('a' :. 'b' :. 'c' :. Nil)
+-- [3,2,1]
+--
+-- >>> id <<= (1 :. 2 :. 3 :. 4 :. Nil)
+-- [[1,2,3,4],[2,3,4],[3,4],[4]]
+--
+-- >>> reverse <<= ((1 :. 2 :. 3 :. Nil) :. (4 :. 5 :. 6 :. Nil) :. Nil)
+-- [[[4,5,6],[1,2,3]],[[4,5,6]]]
+instance Extend List where
+  (<<=) :: (List a -> b) -> List a -> List b
+  (<<=) _ Nil = Nil 
+  (<<=) f l@(_ :. xs) = (f l) :. (f <<= xs) 
+
+
+-- | Implement the @Extend@ instance for @Optional@.
+--
+-- >>> id <<= (Full 7)
+-- Full (Full 7)
+--
+-- >>> id <<= Empty
+-- Empty
+instance Extend Optional where
+  (<<=) ::
+    (Optional a -> b)
+    -> Optional a
+    -> Optional b
+  (<<=) f Empty = Empty
+  (<<=) f (Full a) = Full (f (Full a))
+
+-- | Duplicate the functor using extension.
+--
+-- >>> cojoin (ExactlyOne 7)
+-- ExactlyOne (ExactlyOne 7)
+--
+-- >>> cojoin (1 :. 2 :. 3 :. 4 :. Nil)
+-- [[1,2,3,4],[2,3,4],[3,4],[4]]
+--
+-- >>> cojoin (Full 7)
+-- Full (Full 7)
+--
+-- >>> cojoin Empty
+-- Empty
+cojoin :: Extend f => f a -> f (f a)
+cojoin f = id <<= f
