d9e42ad5a1acb445f894a77ad78f70f8a1a956c4
tkuriyama <taro.kuriyama@gmail.com>
2020-10-13 12:04:41 -0400

    adding Compose

---
+++ fp-course/Compose.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Course.Compose where
+
+import Course.Core
+import Course.Functor
+import Course.Applicative
+import Course.Monad
+
+-- Exactly one of these exercises will not be possible to achieve. Determine which.
+
+newtype Compose f g a = Compose (f (g a))
+
+-- Implement a Functor instance for Compose
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  (<$>) f (Compose g) = Compose ((f <$>) <$> g) 
+
+instance (Applicative f, Applicative g) =>
+  Applicative (Compose f g) where
+-- Implement the pure function for an Applicative instance for Compose
+  pure = Compose . pure . pure
+  (<*>) (Compose f) (Compose a) = Compose (lift2 (<*>) f a)
+
+instance (Monad f, Monad g) =>
+  Monad (Compose f g) where
+-- Implement the (=<<) function for a Monad instance for Compose
+  (=<<) = undefined
+    -- error "todo: Course.Compose (<<=)#instance (Compose f g)"
