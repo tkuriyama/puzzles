3fb553edc71c1535e8b2cdd684328bc23923058e
tkuriyama <taro.kuriyama@gmail.com>
2021-07-30 11:05:00 -0400

    Adding run_in_executor blocking function

--- asyncio/hello_world/hello_world2.py
+++ asyncio/hello_world/hello_world2.py
@@ -6,9 +6,18 @@ async def main():
     await asyncio.sleep(1.0)
     print(f"{time.ctime()} Goodbye!")
 
+def blocking():
+    time.sleep(0.5)
+    print(f"{time.ctime()} Hello from a thread!")
+
+
+
 loop = asyncio.get_event_loop()
 task = loop.create_task(main())
+
+loop.run_in_executor(None, blocking)
 loop.run_until_complete(task)
+
 pending = asyncio.all_tasks(loop=loop)
 for task in pending:
     task.cancel()
