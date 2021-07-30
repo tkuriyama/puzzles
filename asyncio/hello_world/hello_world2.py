2fc39f9a820e83732cfe972d7fb0e3716f68fdb2
tkuriyama <taro.kuriyama@gmail.com>
2021-07-30 10:52:39 -0400

    Hello-ish WOrld

---
+++ asyncio/hello_world/hello_world2.py
@@ -0,0 +1,17 @@
+import asyncio
+import time
+
+async def main():
+    print(f"{time.ctime()} Hello!")
+    await asyncio.sleep(1.0)
+    print(f"{time.ctime()} Goodbye!")
+
+loop = asyncio.get_event_loop()
+task = loop.create_task(main())
+loop.run_until_complete(task)
+pending = asyncio.all_tasks(loop=loop)
+for task in pending:
+    task.cancel()
+group = asyncio.gather(*pending, return_exceptions=True)
+loop.run_until_complete(group)
+loop.close()
